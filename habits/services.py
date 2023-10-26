565741b850bcba75f1b9552f4df02f536c1517e9
Nikolai <kolamba.r@gmail.com>
2023-10-26 05:34:16 +0300

    task_check_schedules and task_check_periods now work as it expected. added some comments

--- habits/services.py
+++ habits/services.py
@@ -1,25 +1,32 @@
+import asyncio
+
 from habits.models import Habit
 from telegram_bot.send_message_bot import send_to_tg
 
 
 def create_message(habit: Habit):
     """
-    Функция получает привычку и делает из нее сообщение для напоминания за 15 мин и сообщение для призыва к действию
+    Функция получает привычку и делает из нее сообщение для напоминания призыва к действию
     :param habit:
     :return:
     """
-    reminder_message = f'Через 15 минут наступит время привычки {habit.action}. Место - {habit.place}'
     call_to_act = f'Время для привычки {habit.action} в месте {habit.place}. Время на выполнение {habit.time_to_done}\n'
 
     # За полезной привычкой следует либо вознаграждение, либо приятная привычка
     if habit.reward:
         call_to_act += f'После привычки вас ждет {habit.reward}!'
     else:
-        call_to_act += f'После привычки вас ждет {habit.rewarding_habit}!'
+        call_to_act += f'После привычки вас ждет {habit.rewarding_habit.action}!'
 
-    return reminder_message, call_to_act
+    return call_to_act
 
 
 def send_tg_message(habit, token):
+    """ Объединяет в себе функции"""
     message = create_message(habit)
-    send_to_tg(token, habit.owner.telegram_id, message)
+    asyncio.run(send_to_tg(token, habit.owner.telegram_id, message))
+
+
+def set_last_event(period, exact_moment):
+    period.last_event = exact_moment
+    period.save()
--- habits/tasks.py
+++ habits/tasks.py
@@ -8,53 +8,59 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 django.setup()
 
 from habits.models import Habit, Schedule
-from habits.services import send_tg_message
-
+from habits.services import send_tg_message, set_last_event
 
 day_of_week = {
-    0: Schedule.mon,
-    1: Schedule.tue,
-    2: Schedule.wed,
-    3: Schedule.th,
-    4: Schedule.fri,
-    5: Schedule.sat,
-    6: Schedule.sun,
-
+    0: 'mon',
+    1: 'tue',
+    2: 'wed',
+    3: 'th',
+    4: 'fri',
+    5: 'sat',
+    6: 'sun',
 }
 
 
 def task_check_schedules():
-    print('asd')
-    current_day = datetime.today().weekday()
+    """ Задача для отправки напоминаний о привычках, завязанных на расписанию"""
+    current_day = datetime.today().weekday()  # получает день недели
+    # получает время в часах и минутах в формате datetime.time
     time_with_minutes = datetime.strptime(datetime.today().strftime('%H:%M'), '%H:%M').time()
 
+    # получает те привычки, которые связаны с объектом Schedule
     habits_on_schedule = Habit.objects.filter(on_schedule__isnull=False)
 
-    print(habits_on_schedule)
     for habit in habits_on_schedule:
-        print(habit.on_schedule.day_of_week[current_day])
-        # проверяет, пришло ли время отправлять
-        if habit.on_schedule.day_of_week[current_day] == time_with_minutes:
-            send_tg_message(habit, settings.TELEGRAM_TOKEN)
+        field_name = day_of_week.get(current_day)  # берет из словаря название поля соответствующее текущему дню недели
+        # Проверяет, есть ли у этого Schedule поле, отвечающее за этот день недели. Если есть то получает его значени
+        if hasattr(habit.on_schedule, field_name):
+            field_value = getattr(habit.on_schedule, field_name)
+
+            # Сравнивает
+            if field_value == time_with_minutes:
+                send_tg_message(habit, settings.TELEGRAM_TOKEN)
 
 
 def task_check_periods():
+    """ Задача для отправки напоминаний о привычках, завязанных на периодичности"""
     exact_moment = datetime.today()
 
+    # получает те привычки, которые связаны с объектом Habit
     periodic_habits = Habit.objects.filter(periodic__isnull=False)
     for habit in periodic_habits:
 
-        period = habit.period
+        period = habit.periodic
         # проверяет рабочее ли это время для этого расписания
-        if period.start < exact_moment < period.end:
-
+        if period.start < exact_moment.time() < period.end:
             # проверяет, отправлялось ли уведомление
             if not period.last_event:
                 send_tg_message(habit, settings.TELEGRAM_TOKEN)
+                set_last_event(period, exact_moment)
             else:
                 # проверяет, что прошло достаточно времени после прошлой отправки
                 if exact_moment - datetime.combine(datetime.today(), period.last_event) >= period.period:
                     send_tg_message(habit, settings.TELEGRAM_TOKEN)
+                    set_last_event(period, exact_moment)
 
 
 if __name__ == '__main__':
